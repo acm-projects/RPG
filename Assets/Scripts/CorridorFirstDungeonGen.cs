f8a9eb0b8d943ec90177ee274edfdc31a9d1bd03
professorjamm <faizaan.siddiqui015@gmail.com>
2024-02-28 04:18:30 -0600

    Updated Hallways

--- Assets/Scripts/CorridorFirstDungeonGen.cs
+++ Assets/Scripts/CorridorFirstDungeonGen.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class CorridorFirstDungeonGen : SimpleRandomWalkDungeonGen
@@ -25,7 +27,7 @@ public class CorridorFirstDungeonGen : SimpleRandomWalkDungeonGen
         HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPositions = new HashSet<Vector2Int>();
 
-        CreateCorridors(floorPositions, potentialRoomPositions);
+        List<List<Vector2Int>> corridors = CreateCorridors(floorPositions, potentialRoomPositions);
 
         HashSet<Vector2Int> roomPositions = CreateRooms(potentialRoomPositions);
 
@@ -34,6 +36,13 @@ public class CorridorFirstDungeonGen : SimpleRandomWalkDungeonGen
         CreateRoomsAtDeadEnd(deadEnds, roomPositions);
 
         floorPositions.UnionWith(roomPositions);
+
+        for (int i = 0; i < corridors.Count; i++)
+        {
+            //corridors[i] = IncreaseCorridorSizeByOne(corridors[i]);
+            corridors[i] = IncreaseCorridorSizeByThree(corridors[i]);
+            floorPositions.UnionWith(corridors[i]);
+        }
 
         tilemapVisualizer.PaintFloorTiles(floorPositions);
         WallGen.CreateWalls(floorPositions, tilemapVisualizer);
@@ -87,17 +96,79 @@ public class CorridorFirstDungeonGen : SimpleRandomWalkDungeonGen
         return roomPositions;
     }
 
-    private void CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
+    private List<List<Vector2Int>> CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomPositions)
     {
         var currentPosition = startPosition;
         potentialRoomPositions.Add(currentPosition); //at each end of a corridor a room gens 
+        List<List<Vector2Int>> cor = new List<List<Vector2Int>>();
 
         for (int i = 0; i < corridorCount; i++)
         {
             var corridor = ProceduralGenerationAlgorithms.RandomWalkCorridor(currentPosition, corridorLength); //will generate corrdier pos
+            cor.Add(corridor);
             currentPosition = corridor[corridor.Count - 1]; //setting current pos to last pos so that corriders are connected 
             potentialRoomPositions.Add(currentPosition);
             floorPositions.UnionWith(corridor);
         }
+        return cor;
     }
+
+    public List<Vector2Int> IncreaseCorridorSizeByThree(List<Vector2Int> corridor)
+    {
+        List<Vector2Int> newCor = new List<Vector2Int>();
+        for (int i = 1; i < corridor.Count; i++)
+            for (int x = -1; x < 2; x++)
+                for (int y = -1; y < 2; y++)
+                    newCor.Add(corridor[i - 1] + new Vector2Int(x, y));
+        return newCor;
+    }
+
+    //OLD METHOD OF EXPANDING CORRIDOR SIZE
+    /*
+    public List<Vector2Int> IncreaseCorridorSizeByOne(List<Vector2Int> corridor)
+    {
+        //this will have duplicate values, the original corridor values + the new neighboring one
+
+        List<Vector2Int> newCor = new List<Vector2Int>();
+        Vector2Int prevDirection = Vector2Int.zero;
+        //starting from 2nd pos in the list, as subtracting the 2nd from the 1st gets the direction 
+        for (int i = 1; i < corridor.Count; i++)
+        {
+            Vector2Int directionFromCell = corridor[i] - corridor[i - 1];
+            if (prevDirection != Vector2Int.zero && directionFromCell != prevDirection)
+            {
+                //for handling corners, making a 3 by 3
+                for (int x = -1; x < 2; x++)
+                    for (int y = -1; y < 2; y++)
+                        newCor.Add(corridor[i - 1] + new Vector2Int(x, y));
+                prevDirection = directionFromCell;
+            }
+            else
+            {
+                //if not a corner, add a new single cell in the direction + 90 degrees 
+                Vector2Int newCorTileOffset = GetDirection90From(directionFromCell);
+                newCor.Add(corridor[i - 1]);
+                newCor.Add(corridor[i - 1] + newCorTileOffset);
+                prevDirection = directionFromCell;
+            }
+        }
+        return newCor;
+    }
+
+    private Vector2Int GetDirection90From(Vector2Int direction)
+    {
+        if (direction == Vector2Int.up)
+            return Vector2Int.right;  //90 degrees clockwise
+        if (direction == Vector2Int.right)
+            return Vector2Int.down;
+        if (direction == Vector2Int.down)
+            return Vector2Int.left;
+        if (direction == Vector2Int.left)
+            return Vector2Int.up;
+        return Vector2Int.zero; //handle unexpected input
+    }
+    */
+
+
+
 }
